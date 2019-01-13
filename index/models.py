a43bac494228a2ba6ab73a151a812b4dbed4f410
Administrator <Siriusmm>
2019-01-13 21:22:32 +0800

    models创建

--- index/models.py
+++ index/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_name=models.CharField(max_length=64,unique=True)
+    user_password=models.CharField(max_length=128)
+    user_email=models.EmailField()
+
+class Notes(models.Model):
+    notes_user_name=models.ForeignKey(User,on_delete=models.DO_NOTHING)
+    note_content=models.TextField()
+    notes_create_date=models.DateTimeField(auto_now_add=True)
+    note_update_date=models.DateTimeField(auto_now=True)
--- index/views.py
+++ index/views.py
@@ -6,12 +6,18 @@ def index_views(request):
     if 'uname' in request.session or 'uname' in request.COOKIES:
         return render(request,'index.html')
     else:
-        return HttpResponseRedirect('/login/')
+        return HttpResponseRedirect('/login')
 
 def login_views(request):
     if request.method=='GET':
-        return render(request,'login.html')
+        if 'uname' in request.session or 'uname' in request.COOKIES:
+            return render(request, 'index.html')
+        else:
+            return render(request,'login.html')
     else:
+        uname=request.POST.get('uname',None)
+        upassword=request.POST.get('upassword',None)
+        form=Remark
         pass
 
 def register_views(request):
